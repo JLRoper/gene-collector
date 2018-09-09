3d3d97e9c15ca6ae5cb7f5633478a437d5668667
JLRoper <JacobLRoper@gmail.com>
2018-09-09 12:21:12 -0400

    clean

--- PowerShell/testWeb.ps1
+++ PowerShell/testWeb.ps1
@@ -1,14 +1,20 @@
 $global:ie = New-Object -com "InternetExplorer.Application"
 $global:ie.Navigate("about:blank")
 $global:ie.visible = $true
-. C:\Users\Jake\Desktop\PowerShell\functions.ps1
+. C:\Users\Jacob\Documents\GitHub\gene-collector\PowerShell\functions.ps1
+ 
 
 
-
-<#NavigateTo "http://www.google.com/advanced_search" 
+<# 
+NavigateTo "http://www.google.com/advanced_search" 
 SetElementValueByName "as_oq" "Unisys Fenix PLDA"
 SetElementValueByName "as_oq" "Unisys Fenix PLDA"
-ClickElementByTagName "input" "14" #>
-NavigateTo "http://www.herbal-salvation.com/shop/soap-ingredients/plantation-maeng-da/" 
+ClickElementByTagName "input" "14" 
+#>
+
+NavigateTo "http://www.herbal-salvation.com/shop/soap-ingredients/green-vein-bali/" 
 SetElementValueByName "quantity" "2"
+$dropdown = $ie.Document.getElementById("pa_size")
+($dropdown | where {$_.innerHTML -eq "100"}).Selected = $true
+#SetElementValueByName "attribute_pa_size" "100"
 ClickElementByTagName "button" "2" 
